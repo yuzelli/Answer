565a6735c0d39cfe3d1eb2c5ec0924c0c58ab610
yuzelli <zsl5155@qq.com>
2018-07-10 13:58:56 +0800

    text01 in put

--- app/src/main/java/com/example/yuzelli/answer/AnswerActivity.java
+++ app/src/main/java/com/example/yuzelli/answer/AnswerActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -39,6 +40,8 @@ public class AnswerActivity extends Activity implements View.OnClickListener {
         initData();
         initView();
         setData();
+
+        Log.d("--->","ddd");
     }
 
 
